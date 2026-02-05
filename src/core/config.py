6b90057adef954a8c21d390e0b8b1fa951dcde90
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-02-06 01:47:01 +0530

    feat(config): configure LLM environment vars

--- src/core/config.py
+++ src/core/config.py
@@ -5,11 +5,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     # Scraping
     SCRAPING_BASE_URL: str = "https://help.zipboard.co"
+    # LLM
+    GROQ_API_KEY: str
+    GROQ_BASE_URL: str = "https://api.groq.com/openai/v1"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 @lru_cache()
 def get_config() -> Settings:
-    return Settings()
+    return Settings() # type: ignore
 
 env_settings = get_config()
