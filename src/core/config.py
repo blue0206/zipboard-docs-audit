9d61ce039e443af7ec0a5af00852fcb5074438c0
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-02-05 08:28:54 +0530

    feat(config): add env config settings using pydantic-settings

---
+++ src/core/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # Scraping
+    SCRAPING_BASE_URL: str = "https://help.zipboard.co"
+    SCRAPING_CONCURRENT_REQUESTS: int = 2
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+@lru_cache()
+def get_config() -> Settings:
+    return Settings()
+
+env_settings = get_config()
