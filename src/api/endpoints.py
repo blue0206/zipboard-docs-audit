83e2b06c6e60b0b5a49bf000b0adfc566bd8b1f0
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-02-10 17:32:44 +0530

    feat(api): enhance error handling and structure in run_pipeline function

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -61,64 +61,69 @@ async def run_pipeline(
     Runs the complete pipeline for scraping, analyzing, and updating Google Sheets.
     """
 
-    # 1. ------------------Scraping-----------------------
-    scraped_data = await run_scraper(concurrency=concurrency, limit=limit)
+    try:
+        # ------------------Scraping-----------------------
+        scraped_data = await run_scraper(concurrency=concurrency, limit=limit)
 
-    # 2. -------------------Article Analysis------------------------------
+        # -------------------Article Analysis------------------------------
+        # Normalize scraped data and pass for LLM ingestion to generate insights and identify gaps.
+        normalized_articles = normalize_scraped_articles(scraped_data)
+        article_analysis_result = await analyze_articles(normalized_articles)
 
-    # Normalize scraped data and pass for LLM ingestion to generate insights and identify gaps.
-    normalized_articles = normalize_scraped_articles(scraped_data)
-    article_analysis_result = await analyze_articles(normalized_articles)
-
-    # Normalize analyzed data and initial normalized article data into a structure fit for
-    # spreadsheet display.
-    articles_catalogue = normalize_analyzed_articles_to_catalogue(
-        analyzed_articles=article_analysis_result, articles=normalized_articles
-    )
-
-    # Flatten normalized data into dicts and update spreadsheet.
-    flattened_articles = flatten_articles_catalogue(articles_catalogue)
-    await run_in_threadpool(
-        update_google_sheets, flattened_articles, "Articles Catalogue"
-    )
-
-    # 3. ---------------------------Gap Analysis---------------------------------
-
-    if gap_analysis:
-        # Normalize analyzed article data and the finalized article catalogue into LLM-ready
-        # input for Gap Analysis.
-        gap_analysis_input = generate_gap_analysis_input(
-            scraped_data=scraped_data, articles=articles_catalogue
+        # Normalize analyzed data and initial normalized article data into a structure fit for
+        # spreadsheet display.
+        articles_catalogue = normalize_analyzed_articles_to_catalogue(
+            analyzed_articles=article_analysis_result, articles=normalized_articles
         )
-        gap_analysis_result = await run_gap_analysis(gap_analysis_input)
 
-        # Flatten gap analysis results into dict and update spreadsheet.
-        flattened_gaps = flatten_gap_analysis_result(gap_analysis_result)
-        await run_in_threadpool(update_google_sheets, flattened_gaps, "Gap Analysis")
+        # Flatten normalized data into dicts and update spreadsheet.
+        flattened_articles = flatten_articles_catalogue(articles_catalogue)
+        await run_in_threadpool(
+            update_google_sheets, flattened_articles, "Articles Catalogue"
+        )
 
-    # 4. ----------------------------Competitor Analysis-----------------------------
+        # ---------------------------Gap Analysis---------------------------------
 
+        if gap_analysis:
+            # Normalize analyzed article data and the finalized article catalogue into LLM-ready
+            # input for Gap Analysis.
+            gap_analysis_input = generate_gap_analysis_input(
+                scraped_data=scraped_data, articles=articles_catalogue
+            )
+            gap_analysis_result = await run_gap_analysis(gap_analysis_input)
+
+            # Flatten gap analysis results into dict and update spreadsheet.
+            flattened_gaps = flatten_gap_analysis_result(gap_analysis_result)
+            await run_in_threadpool(update_google_sheets, flattened_gaps, "Gap Analysis")
+    except Exception as e:
+        print(f"Error occurred in Pipeline: {str(e)}")
+
+    # We allow competitor analysis to run as it does not rely on scraped/analyzed data.
+    # ----------------------------Competitor Analysis-----------------------------
     if competitor_analysis:
-        # Perform competitor analysis. It requires the same context as gap analysis.
-        competitor_analysis_result = await run_competitor_analysis()
+        try:
+            # Perform competitor analysis. It requires the same context as gap analysis.
+            competitor_analysis_result = await run_competitor_analysis()
 
-        # We represent competitor analysis in 2 tables:
-        # a. Competitor Comparison
-        # b. Insights for zipBoard based on competitor analysis
-        # Hence, we extract and flatten both data into separate vars and update sheets.
-        flattened_competitor_comparison = flatten_competitor_comparison(
-            competitor_analysis_result
-        )
-        flattened_competitor_analysis_insights = flatten_competitor_analysis_insights(
-            competitor_analysis_result
-        )
-        await run_in_threadpool(
-            update_google_sheets,
-            flattened_competitor_comparison,
-            "Competitor Comparison",
-        )
-        await run_in_threadpool(
-            update_google_sheets,
-            flattened_competitor_analysis_insights,
-            "Strategic Insights & Recommendations",
-        )
+            # We represent competitor analysis in 2 tables:
+            # a. Competitor Comparison
+            # b. Insights for zipBoard based on competitor analysis
+            # Hence, we extract and flatten both data into separate vars and update sheets.
+            flattened_competitor_comparison = flatten_competitor_comparison(
+                competitor_analysis_result
+            )
+            flattened_competitor_analysis_insights = flatten_competitor_analysis_insights(
+                competitor_analysis_result
+            )
+            await run_in_threadpool(
+                update_google_sheets,
+                flattened_competitor_comparison,
+                "Competitor Comparison",
+            )
+            await run_in_threadpool(
+                update_google_sheets,
+                flattened_competitor_analysis_insights,
+                "Strategic Insights & Recommendations",
+            )
+        except Exception as e:
+            print(f"Error occurred in Competitor Analysis: {str(e)}")
